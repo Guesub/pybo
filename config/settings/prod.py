32fd3c416938ddd630493aa0fda1ddd2a8491500
guesubKim <guesub@gmail.com>
2021-05-26 16:13:01 +0900

    logging 확인

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS=['13.125.185.206', 'qsub.co.kr']
+ALLOWED_HOSTS=['15.165.224.200', 'qsub.co.kr']
 DEBUG=False
 STATIC_ROOT= BASE_DIR /'static/'
 STATICFILES_DIRS=[]
