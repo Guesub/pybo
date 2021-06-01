31007cfaefd1b60e644a6137aa0fc2a37af80e4f
guesubKim <guesub@gmail.com>
2021-06-02 00:15:13 +0900

    Question 카테고리 구현

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.165.224.200']
+ALLOWED_HOSTS = ['15.165.244.200']
 
 
 # Application definition
